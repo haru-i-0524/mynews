695b04f92ad536afb30b95e3cc43515373c6fc28
HarukaImaeda <haruka.imaeda.0425@gmail.com>
2020-09-12 12:13:57 +0000

    PHP/Laravel 13 kadai04

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -10,6 +10,64 @@
         <div class="row">
             <div class="col-md-8 mx-auto">
                 <h2>Myプロフィール</h2>
+                <form action="{{
+                action('Admin\ProfileController@create') }}" method="post">
+                    
+                   @if (count($errors) > 0)
+                        <ul>
+                            @foreach($errors->all() as $e)
+                                <li>{{ $e }}</li>
+                            @endforeach
+                        </ul>
+                    @endif
+                    
+                    <!-- 氏名(name) -->
+                    <div class="form-group row">
+                        <label class="col-md-2">{{ __('messages.Name') }}</label>
+                        
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="name" value="{{ old('name') }}">
+                            
+                        </div>
+                    </div>
+                    
+                    <!-- 性別(gender) -->
+                    <div class="form-group row">
+                        <label class="col-md-2">{{ __('messages.Gender') }}</label>
+                        
+                        <div class="col-md-2">    
+                            <input type="radio" name="gender" value="male">{{ __('messages.Male') }}
+                       
+                       </div>
+                       <div class="col-md-2">    
+                            <input type="radio" name="gender" value="female">{{ __('messages.Female') }}
+                       
+                       </div>
+                    </div>
+                    
+                    <!-- 趣味(hobby) -->
+                    <div class="form-group row">
+                        <label class="col-md-2">{{ __('messages.Hobby') }}</label>
+                        
+                        <div class="col-md-10">
+                            <textarea class="form-control" name="hobby" rows='10'></textarea>
+                        </div>
+                    </div>
+                    
+                    <!-- 自己紹介(introduction) -->
+                    <div class="form-group row">
+                        <label class="col-md-2">{{ __('messages.Introduction') }}</label>
+                        
+                        <div class="col-md-10">
+                            <textarea class="form-control" name="introduction" rows='10'></textarea>
+                            
+                        </div>
+                    </div>
+                    {{ csrf_field() }}
+                    <input type="submit" class="btn btn-primary" value="登録">
+                    
+                    
+                </form>
             </div>
         </div>
     </div>
